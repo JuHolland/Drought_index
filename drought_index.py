0b412026d0acce8f447ef5c7c51648482c2e89f5
JuHolland <juliette.holland@wfp.org>
2022-03-01 16:29:33 +0100

    rfh to r1h

--- drought_index.py
+++ drought_index.py
@@ -17,15 +17,15 @@ def main(begin:str, end:str):
 	# Loading Data #
 	################
 
-	path_rfh = 'Data/rfh'
+	path_r1h = 'Data/r1h'
 	path_vim = 'Data/vim'
 	path_tda = 'Data/tda'
 	path_dlx = 'Data/dlx'
 	path_sos = 'Data/sos'
-	path_smoothed_rfh = 'Data/afsr1h_avgmmd3_smooth'
+	path_smoothed_r1h = 'Data/afsr1h_avgmmd3_smooth'
 
-	da_rfh = create_xarray(path_rfh + '/*.tif')
-	print('downloaded rfh')
+	da_r1h = create_xarray(path_r1h + '/*.tif')
+	print('downloaded r1h')
 	da_vim = create_xarray(path_vim + '/*.tif')
 	print('downloaded vim')
 	da_tda = create_xarray(path_tda + '/*.tif')
@@ -35,25 +35,25 @@ def main(begin:str, end:str):
 	da_sos = create_xarray(path_sos + '/*.tif')
 	print('downloaded sos')
 
-	das = [da_rfh, da_vim, da_tda, da_dlx]
+	das = [da_r1h, da_vim, da_tda, da_dlx]
 
 
 	###########
 	# Weights #
 	###########
 
-	# Weights computed from smoothed rfh data
-	da_rfh_smooth = create_xarray(path_smoothed_rfh + '/*.rst', avg = True)
+	# Weights computed from smoothed r1h data
+	da_r1h_smooth = create_xarray(path_smoothed_r1h + '/*.rst', avg = True)
 
 	# Selecting months of interest
-	da_rfh_smooth_sel = da_rfh_smooth.sel(time = get_months(begin, end))
+	da_r1h_smooth_sel = da_r1h_smooth.sel(time = get_months(begin, end))
 
 	# Creating mask
-	ws = da_rfh_smooth_sel.sum('time')
+	ws = da_r1h_smooth_sel.sum('time')
 	w_mask = ws.where(ws == 0, 1)
 
 	# Computing weights
-	weights = da_rfh_smooth_sel / ws
+	weights = da_r1h_smooth_sel / ws
 	weights = weights.rename({'time': 'month'})
 
 
@@ -68,7 +68,7 @@ def main(begin:str, end:str):
 
 	Qs = []
 
-	# RFH, VIM, TDA, DLX
+	# R1H, VIM, TDA, DLX
 	for i,da in tqdm(enumerate(das)):
 	    
 	    # Deriving percentile
@@ -108,17 +108,17 @@ def main(begin:str, end:str):
 
 
 	# QMULTI
-	
-	[Qrfh, Qvim, Qtda, Qdlx] = Qs
 
-	coef_rfh = 0.15
+	[Qr1h, Qvim, Qtda, Qdlx] = Qs
+
+	coef_r1h = 0.15
 	coef_vim = 0.15
 	coef_tda = 0.25
 	coef_dlx = 0.25
 	coef_sos = 0.2
 
-	Qmulti = Qrfh.copy()
-	Qmulti.values = coef_rfh * Qrfh.values + coef_vim * Qvim.values + coef_tda * Qtda.values + coef_dlx * Qdlx.values + coef_sos * Qsos.values
+	Qmulti = Qr1h.copy()
+	Qmulti.values = coef_r1h * Qr1h.values + coef_vim * Qvim.values + coef_tda * Qtda.values + coef_dlx * Qdlx.values + coef_sos * Qsos.values
 
 
 	##################
